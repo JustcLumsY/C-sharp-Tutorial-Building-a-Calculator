894fb719df401d5db8d381fc02d76da113ff568f
John Espen Thoresen <84008816+JustcLumsY@users.noreply.github.com>
2022-01-05 12:40:06 +0100

    Add project files.

---
+++ Building a Calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Building_a_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            //int num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter another number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine()); 
+
+            Console.WriteLine(num1 + num2);
+        }
+    }
+}
